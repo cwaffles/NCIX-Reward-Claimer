8599164efb77582cd68f4046c6475a6639ed402b
Brad Kilshaw <brad@etylogic.com>
2011-03-19 04:09:07 -0400

    added in recaptcha. yes my keys are in here. yes this is a public repo. no i don't care. yes i probably should.

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
   
   require 'config.php';
+  require_once('recaptchalib.php');
   
+   $privatekey = "6LdUjcISAAAAAEaucmnU214RKiN48ByjRlpuH_Gm";
+   if(isset($_POST["recaptcha_challenge_field"]) && $_POST["recaptcha_response_field"]) {
+     $resp = recaptcha_check_answer (
+          $privatekey
+        , $_SERVER["REMOTE_ADDR"]
+        , $_POST["recaptcha_challenge_field"]
+        , $_POST["recaptcha_response_field"]
+      );  
+   }
+   
+
   $user_created = "";
   $valid_email = TRUE;
+  $valid_captcha = TRUE;
   
   if(isset($_POST['email'])) {
  
@@ -13,11 +26,15 @@
     
     if(isset($match[0])) {
       if($match[0] == $email) {
-        $user = new User($email);
+        if ($resp->is_valid) {
+          $user = new User($email);
 
-        if($user->create()){
-          $user_created = TRUE;
-        }      
+          if($user->create()){
+            $user_created = TRUE;
+          }
+        } else {
+          $valid_captcha = FALSE;
+        }    
       }
     } else {
       $valid_email = FALSE;
@@ -35,6 +52,9 @@
     <?php if($valid_email === FALSE): ?>
         <div style="color: #FFF; background: red; padding: 5px; margin: 5px; font-size: 1.2em; text-align: center;">Invalid Email Address!</div>
     <?php endif; ?>
+    <?php if($valid_captcha === FALSE): ?>
+        <div style="color: #FFF; background: red; padding: 5px; margin: 5px; font-size: 1.2em; text-align: center;">Invalid Captcha!</div>
+    <?php endif; ?>
         
     <?php if($user_created === TRUE): ?>
       <div style="color: #FFF; background: green; padding: 5px; margin: 5px; font-size: 1.2em; text-align: center;"> <?php echo $email; ?> has been added to the auto-claimer!</div>
@@ -43,6 +63,11 @@
     <?php else: ?>
       <form action="" method="POST">
         Email: <input type="text" name="email" /><br />
+        <?php
+          require_once('recaptchalib.php');
+          $publickey = "6LdUjcISAAAAAHmDqSBdZd9_jgCLpeJ3gHqXrRg4";
+          echo recaptcha_get_html($publickey);
+        ?>
         <input type="submit" value="Add to List" />
       </form>
 
